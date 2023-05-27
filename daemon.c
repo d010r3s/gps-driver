0eb632c58c625c31a60cb53c8090c31de59e9085
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-05-28 01:48:51 +0300

    Created

---
+++ daemon.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "gps.h"
+
+int main() {
+    if (gps_driver_init() != 0) {
+        printf("Error\n");
+        return 1;
+    }
+
+    while (1) {
+        float latitude, longitude, altitude;
+        if (data(&latitude, &longitude, &altitude) != 0) {
+            printf("Error\n");
+            continue; 
+        }
+        printf("Coordinates:\n");
+        printf("Latitude: %f\n Longitude: %f\n Altitude: %f\n", latitude, longitude, altitude);
+        sleep(1);
+    }
+    cleanup();
+    return 0;
+}
