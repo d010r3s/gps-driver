c91ab9462319fb48cb4b6cfc80d594c3771929a6
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-05-28 02:31:50 +0300

    Update daemon.c

--- daemon.c
+++ daemon.c
@@ -8,38 +8,22 @@ int main() {
         return 1;
     }
 
+    int mode = 1;
+
     while (1) {
         float latitude, longitude, altitude;
         if (data(&latitude, &longitude, &altitude) != 0) {
             printf("Error\n");
             continue; 
         }
+
         printf("Coordinates:\n");
         printf("Latitude: %f\n Longitude: %f\n Altitude: %f\n", latitude, longitude, altitude);
         sleep(1);
-        if (mode == 1) {
-            printf("Mode 1:\n");
-            if (altitude > 1000) {
-                printf("Altitude is higher than 1km\n");
-            //
-            }
-            else {
-            printf("Altitude is lower than 1km\n");
-            //
-            }
-        }
-        else if (mode == 2) {
-            printf("Mode 2:\n");
-            if (latitude > 50.0 && longitude < 50.0) {
-                printf("Idk something\n");
-            // 
-            }
-            else {
-                printf("Something else ig\n");
-            //
-            }
-        }
+
+        process_coordinates(latitude, longitude, altitude, mode);
     }
+
     cleanup();
     return 0;
 }
