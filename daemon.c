fc811f23b0a5ad5131ea0b31e1afd69aedf2d5d1
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-05-28 02:01:33 +0300

    Updated
    modes added

--- daemon.c
+++ daemon.c
@@ -17,6 +17,28 @@ int main() {
         printf("Coordinates:\n");
         printf("Latitude: %f\n Longitude: %f\n Altitude: %f\n", latitude, longitude, altitude);
         sleep(1);
+        if (mode == 1) {
+            printf("Mode 1:\n");
+            if (altitude > 1000) {
+                printf("Altitude is higher than 1km\n");
+            //
+            }
+            else {
+            printf("Altitude is lower than 1km\n");
+            //
+            }
+        }
+        else if (mode == 2) {
+            printf("Mode 2:\n");
+            if (latitude > 50.0 && longitude < 50.0) {
+                printf("Idk something\n");
+            // 
+            }
+            else {
+                printf("Something else ig\n");
+            //
+            }
+        }
     }
     cleanup();
     return 0;
