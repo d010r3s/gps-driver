fa94dc2f809c4b2e4355f7373378068057c43ae0
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-05-29 16:27:33 +0300

    Update daemon.c

--- daemon.c
+++ daemon.c
@@ -2,11 +2,10 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <string.h>
 #include <arpa/inet.h>
 #include "gps.h"
 
-#define PORT 1
+#define PORT 8080
 
 int main() {
     if (gps_driver_init() != 0) {
@@ -15,59 +14,42 @@ int main() {
     }
 
     int mode = 1;
-    
+
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
+    if (sockfd < 0) {
         perror("Socket creation failed");
         return 1;
     }
 
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     server_addr.sin_port = htons(PORT);
 
-    if (bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Socket bind failed");
-        close(sockfd);
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed");
         return 1;
     }
-
-    if (listen(sockfd, 1) == -1) {
-        perror("Listen failed");
-        close(sockfd);
-        return 1;
-    }
-
-    struct sockaddr_in client_addr;
-    socklen_t client_len = sizeof(client_addr);
-
-    int clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &client_len);
-    if (clientfd == -1) {
-        perror("Accept failed");
-        close(sockfd);
-        return 1;
-    }
-
-    close(sockfd);
 
     while (1) {
         float latitude, longitude, altitude;
         if (data(&latitude, &longitude, &altitude) != 0) {
             printf("Error\n");
-            continue;
+            continue; 
         }
 
         printf("Coordinates:\n");
         printf("Latitude: %f\nLongitude: %f\nAltitude: %f\n", latitude, longitude, altitude);
         sleep(1);
+
+        send(sockfd, &latitude, sizeof(latitude), 0);
+        send(sockfd, &longitude, sizeof(longitude), 0);
+        send(sockfd, &altitude, sizeof(altitude), 0);
+
         process_coordinates(latitude, longitude, altitude, mode);
-        char message[100];
-        sprintf(message, "Latitude: %f, Longitude: %f, Altitude: %f\n", latitude, longitude, altitude);
-        send(clientfd, message, strlen(message), 0);
     }
 
     cleanup();
-    close(clientfd);
+    close(sockfd);
     return 0;
 }
