db3e3c470029c93ab1147b192e7053286a5e05cc
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-06-03 01:56:48 +0300

    Create application.h
    basic user interaction

---
+++ application.h
@@ -0,0 +1,34 @@
+#include "gps.h"
+#include <stdio.h>
+
+int main() {
+    gps_driver_init();
+
+    while (1) {
+        printf("gps tracker\n");
+        printf("1. start\n");
+        printf("2. stop\n");
+        printf("3. exit\n");
+        printf("enter your choice: ");
+
+        int choice;
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                sleep(1);
+                printf("Tracking started.\n");
+                break;
+            case 2:
+                sleep(1);
+                printf("Tracking stopped.\n");
+                break;
+            case 3:
+                printf("Exiting...\n");
+                return 0;
+            default:
+                printf("invalid choice\n");
+        }
+    }
+    return 0;
+}
