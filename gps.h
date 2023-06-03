91543a2f2f2b45b8a1dfc2141df176cd5630179d
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-06-03 17:56:20 +0300

    Update gps.h

--- gps.h
+++ gps.h
@@ -2,9 +2,17 @@
 #define GPS_DAEMON_H
 
 #include <stdio.h>
-
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <stdlib.h>
 void process_coordinates(float latitude, float longitude, float altitude, int mode);
 int gps_driver_init();
 void cleanup();
-
+float get_latitude(); 
+float get_longitude(); 
+float get_altitude();
+int demon();
 #endif
