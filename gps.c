304814b457eb0d407c5982beef58f1c2d5e9a644
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-06-03 17:56:35 +0300

    Update gps.c

--- gps.c
+++ gps.c
@@ -1,34 +1,49 @@
 #include "gps.h"
+
+float latitude, longitude, altitude;
+float min = 1.0;
+float max = 3000.0;
 void cleanup() {
-    printf("Performing cleanup...\n");
+    printf("performing cleanup...\n");
     sleep(1);
 }
 int gps_driver_init() {
-    printf("Start...\n");
+    printf("start...\n");
     sleep(1); 
     return 0;
 }
-int data(float* latitude, float* longitude, float* altitude) {
-    *latitude = 1.;
-    *longitude = 2.;
-    *altitude = 3.;
+float get_latitude() {
+    srand(time(0));
+    latitude = ((float)rand() / RAND_MAX) * (max - min) + min;
     sleep(1);
-    return 0; 
+    return latitude; 
+}
+float get_longitude() {
+    srand(time(0));
+    longitude = ((float)rand() / RAND_MAX) * (max - min) + min;
+    sleep(1);
+    return longitude;
+}
+float get_altitude() {
+    srand(time(0));
+    altitude = ((float)rand() / RAND_MAX) * (max - min) + min;
+    sleep(1);
+    return altitude;
 }
 void process_coordinates(float latitude, float longitude, float altitude, int mode) {
-    printf("Mode: %d\n", mode);
+    printf("mode: %d\n", mode);
 
     if (mode == 1) {
         if (altitude > 1000) {
-            printf("Altitude is higher than 1km\n");
+            printf("the object is higher than 1km\n");
         } else {
-            printf("Altitude is lower than 1km\n");
+            printf("the object is lower than 1km\n");
         }
     } else if (mode == 2) {
         if (latitude > 50.0 && longitude < 50.0) {
-            printf("Idk something\n");
+            printf("idk something\n");
         } else {
-            printf("Something else ig\n");
+            printf("something else ig\n");
         }
     }
 }
