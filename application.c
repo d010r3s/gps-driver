cecfeb77ce0d37ec8915150b49a23757e65fc3a3
Dolores <104917935+d010r3s@users.noreply.github.com>
2023-06-26 13:58:38 +0300

    Update application.c

--- application.c
+++ application.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <stdlib.h>
 int main() {
     int mode;
     float altitude, latitude, longitude;
@@ -13,35 +20,49 @@ int main() {
 
         
         int choice;
-        char tiime[256],info[256];
+        char tiime[100],info[100],response[100];
         while (1) {
         printf("enter your choice: ");
         scanf("%d", &choice);
         switch (choice) {
             case 1:
                 sleep(1);
-                printf("tracking started. enter mode in config (1-2).\n");
                 // demon();
+                FILE *modes = fopen("modes.txt", "w");
+                fprintf(modes, "1");
+                fclose(modes);
+                modes = fopen("modes.txt","r");
+                fscanf(modes, "%s\n", response);
+                printf("%s", response);
+                fclose(modes);
                 break;
             case 2:
-                
+                modes = fopen("modes.txt", "w");
+                // demon();
+                fprintf(modes, "2");
+                fclose(modes);
+                modes = fopen("modes.txt", "r");
+                fscanf(modes, "%s\n", response);
                 sleep(1);
-                printf("tracking stopped.\n");
+                printf("%s", response);
                 break;
             case 3:
                 sleep(1);
-               // demon();
+                
                 FILE *data = fopen("data.txt", "r");
+                // demon();
                 fscanf(data, "%f %f %f", &latitude, &longitude, &altitude);
                 printf("coordinates:\nlatitude: %f\nlongitude: %f\naltitude: %f\n", latitude, longitude, altitude);
                 fclose(data);
+                break;
             case 4:
                 sleep(1);
                 // demon();
                 FILE *logs = fopen("logs.txt", "r");
-                fscanf(logs, "%s%s", &tiime, &info);
+                fscanf(logs, "%s%s", tiime, info);
                 printf("%s%s", tiime, info);
                 fclose(logs);
+                break;
             case 5:
                 printf("exiting...\n");
                 system("killall -9 file1");
